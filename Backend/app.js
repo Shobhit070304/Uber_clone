55d5e68562836a28fdd55e90f77a1ee4bddd4e4c
Shobhit <sharmashobhit1000@gmail.com>
2025-02-07 16:50:23 +0530

    user model created

--- Backend/app.js
+++ Backend/app.js
@@ -2,9 +2,11 @@ const dotenv = require("dotenv");
 dotenv.config();
 const express = require("express");
 const cors = require("cors");
+const connectToDatabase = require("./db/db");
 const app = express();
 
 app.use(cors());
+connectToDatabase();
 
 app.get("/", (req, res) => {
   res.send("Welcome to Backend!");
